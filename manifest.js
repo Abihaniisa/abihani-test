40a35b4833ada3ba42c93b0c368ad6d070db807e
Abihaniisa <bayeroisa2003@gmail.com>
2026-04-30 00:07:32 +0100

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,20 @@
+{
+    "name": "PWA Test App",
+    "short_name": "PWA Test",
+    "start_url": "/",
+    "display": "standalone",
+    "background_color": "#ffffff",
+    "theme_color": "#b87c4f",
+    "icons": [
+        {
+            "src": "https://hibpuvurlvkuqjawkqlu.supabase.co/storage/v1/object/public/images/logo.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "https://hibpuvurlvkuqjawkqlu.supabase.co/storage/v1/object/public/images/logo.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ]
+}
