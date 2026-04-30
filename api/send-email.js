6dd060d9419fa5c9c7c70b9172a15b425e6edb6c
Abihaniisa <bayeroisa2003@gmail.com>
2026-04-30 17:51:00 +0100

    Add files via upload

--- api/send-email.js
+++ api/send-email.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + process.env.RESEND_API_KEY
+                'Authorization': 'Bearer re_gwvYG6oG_7GbFqxZTBcSQ8gdjYUCFHjQi'
             },
             body: JSON.stringify({
                 from: 'Abihani Express <support@abihaniexpress.com.ng>',
